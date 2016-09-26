ca4b4921a14a25a5de900c8fb3b7dafcfb934c71
Sailor Winkelman <sailorwinkelman@gmail.com>
2016-09-26 16:39:19 -0700

    Add js and jQuery functions to return one of three vactation results.

---
+++ README.md
@@ -0,0 +1,37 @@
+# _Vacation Suggester_
+
+#### _This website contains an interactive quiz form that will give the user a
+suggested vacation destination, based on their answers to a the questions._
+
+#### By _**Sailor Winkelman**_
+
+## Description
+
+_This website contains an interactive quiz form that will give the user a
+suggested vacation destination, based on their answers to a the questions._
+
+## Setup/Installation Requirements
+
+* _Open a web browser_
+* _Type your name in where it says "Enter your name"_
+* _Select answers to all 5 questions_
+*_Click "submit"_
+
+## Technology Used
+
+* _Command Line_
+* _Atom_
+* _Git_
+* _Github_
+* _Google Chrome_
+* _Safari_
+* _HTML_
+* _CSS_
+* _Bootstrap_
+*_Javascript_
+*_JQuery_
+
+## License
+
+Copyright (c) 2016 **_Sailor Winkelman_**
+[MIT License](https://en.wikipedia.org/wiki/MIT_License "Read about MIT here")
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
+    var userName = $("input#user").val();
     var photoChoice = $("input:radio[name=photo]:checked").val();
     var radioChoice = $("input:radio[name=radio]:checked").val();
     var lookChoice = $("input:radio[name=look]:checked").val();
     var vehicleChoice = $("input:radio[name=vehicle]:checked").val();
     var snackChoice = $("input:radio[name=snack]:checked").val();
     var result;
-
-    if (vehicleChoice === "jeep") {
-      result = "COACHELLA";
-    }
-
-  $("#output").text(result);
+    if (vehicleChoice === "jeep" || radioChoice === "rhcp") {
+      result = "COACHELLA.";
+   } else if (photoChoice === "boot"){
+     result = "DOLLYWOOD.";
+   } else if (lookChoice === "practical"){
+    result = "ICE FISHING.";
+   } else if (photoChoice === "fish" && lookChoice === "tasteless"){
+     result = ("COACHELLA.");
+   }
+   $("#suggestion").slideDown();
+   $("#output1").text(userName);
+   $("#output2").text(result);
   });
 });
